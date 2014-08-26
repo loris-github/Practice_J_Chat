479424c0ec59775c70125e5bbfa09d9d8f95d9c6
loris-github <laotao200437@gmail.com>
2014-08-26 10:56:29 +0800

    实现了服务器端向客户端发消息
    服务端新增clients容器保存客户端，修改三处：client.start()，每接收一个就忘容器中放一个，Client类中加入发送方法；finally中加入关闭发送端的处理

--- src/com/test/Chat/ChatServer.java
+++ src/com/test/Chat/ChatServer.java
@@ -3,12 +3,13 @@ package com.test.Chat;
 import java.io.IOException;
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 public class ChatServer {
 
 	boolean started = false;
 	ServerSocket ss = null;
-	
+	List<Client> clients = new ArrayList<Client>();
 	public static void main(String[] args) {
 		new ChatServer().start();
 	}
@@ -23,25 +24,22 @@ public class ChatServer {
 			e1.printStackTrace();
 		}
 		
-		try {			
+		try {
 			while(started){
 				Socket s = ss.accept();
 				Client c = new Client(s);
-				System.out.println("a client connected!");
+				clients.add(c);
+				System.out.println("a client connected! O(∩_∩)O~");
 				new Thread(c).start();
-
-				//dis.close();
 			}
 		} catch (EOFException e) {
-			//e.printStackTrace();
-			System.out.println("Client closed");
+			System.out.println("Client closed ( ^_^ )/~~拜拜");
 		} catch (Exception e){
 			e.printStackTrace();
 		} finally{
 			try {
 				ss.close();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
@@ -50,40 +48,65 @@ public class ChatServer {
 	class Client implements Runnable{
 		private Socket s;
 		private DataInputStream dis = null;
+		private DataOutputStream dos = null;
 		private boolean bConnected = false;
 		public Client (Socket s){
 			this.s = s;
 			try {
-				dis  = new DataInputStream(s.getInputStream());
+				dis = new DataInputStream(s.getInputStream());
+				dos = new DataOutputStream(s.getOutputStream());
 				bConnected  = true;
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		
+		public void send(String str){
+			try {
+				dos.writeUTF(str);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 		@Override
 		public void run() {
 			try{
 				while(bConnected){
 					String str = dis.readUTF();
 					System.out.println(str);
+					
+					for(int i=0;i<clients.size();i++){
+						Client c = clients.get(i);
+						c.send(str);
+					}
+					
+					//以下两种方式需要锁定 所以效率低下，弃用；
+					
+					/*for(Iterator<Client> it = clients.iterator();it.hasNext();){
+						Client c = it.next();
+						c.send (str);
+					}*/
+					
+					/*Iterator<Client> it = clients.iterator();
+					while(it.hasNext()){
+						Client c = it.next();
+						c.send(str);
+					}*/
+					
+					
 				}
 			} catch (EOFException e) {
-				//e.printStackTrace();
-				System.out.println("Client closed");
+				System.out.println("客户端那边断鸟 ╮(╯▽╰)╭");
 			} catch (Exception e){
 				e.printStackTrace();
 			} finally{
 				try{
 					if(dis != null) dis.close();
+					if(dos != null) dos.close();
 					if(s != null) s.close();
 				} catch(IOException e1){
 					e1.printStackTrace();
 				}
 			}
-
-			
 		}
-		
 	}
 }
